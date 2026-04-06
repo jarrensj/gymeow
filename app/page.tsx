6ce7834058c3a6264da0e9e3e9b0bb930ce569c7
Jarren San Jose <sanjose.jarren@gmail.com>
2026-04-06 00:58:21 -0700

    make gymeow coming soon palceholder landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex-1 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">gymeow</h1>
+      <p className="text-xl text-gray-500 mt-2">coming soon</p>
+    </main>
+  );
 }
